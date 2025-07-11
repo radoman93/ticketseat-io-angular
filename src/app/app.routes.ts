f248266046c31a337ee3cfa65ce0e32e0e338f4b
Pavle <radomanp@gmail.com>
2025-07-11 10:59:45 +0200

    refactor: remove unused EditorWithUpdates and SeatLimitDemo components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { EditorWithUpdatesComponent } from './examples/editor-with-updates.component';
 
 export const routes: Routes = [
     {
-        path: 'examples/editor-with-updates',
-        component: EditorWithUpdatesComponent
+
     }
 ];
